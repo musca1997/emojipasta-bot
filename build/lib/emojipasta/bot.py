fee49c73e8b58dfca42f1eaab0587a7de1bda6de
Musca <musturtwig@gmail.com>
2018-09-27 14:01:33 +0800

    ok i wish this time it works

--- build/lib/emojipasta/bot.py
+++ build/lib/emojipasta/bot.py
@@ -276,6 +276,8 @@ class Restricted:
             if (str(ctx.message.author.id) == i):
                 await client.change_presence(game=discord.Game(name=(new_stat)))
                 await client.say("Done.")
+            break
+            
         else:
             await client.say("HAHA CUCKED U DONT HAVE THE PERMISSION TO CHANGE MY STATUS.")
 
